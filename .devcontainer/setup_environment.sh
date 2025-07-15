b7e55e83f4fc6cbd124a7cdc0e31e81fbc2acd00
brstrutt <ben.strutt@hotmail.co.uk>
2025-07-15 21:42:34 +0000

    update devcontainer to make the environment variables persist properly

--- .devcontainer/setup_environment.sh
+++ .devcontainer/setup_environment.sh
@@ -8,21 +8,17 @@ apk add pacman
 ## Update pacman install to allow installing devkitARM
 pacman-key --init
 
-DEVKITPRO=/opt/devkitpro
-DEVKITARM=/opt/devkitpro/devkitARM
-DEVKITPPC=/opt/devkitpro/devkitPPC
-
 pacman-key --recv BC26F752D25B92CE272E0F44F7FD5492264BB9D0 --keyserver keyserver.ubuntu.com
 pacman-key --lsign BC26F752D25B92CE272E0F44F7FD5492264BB9D0
 
 pacman --noconfirm -U https://pkg.devkitpro.org/devkitpro-keyring.pkg.tar.zst
 
 ## Add devkit pro repos
-echo "[dkp-libs]" > /etc/pacman.conf
-echo "Server = https://pkg.devkitpro.org/packages" > /etc/pacman.conf
+echo "[dkp-libs]" >> /etc/pacman.conf
+echo "Server = https://pkg.devkitpro.org/packages" >> /etc/pacman.conf
 
-echo "[dkp-linux-musl]" > /etc/pacman.conf
-echo "Server =  https://pkg.devkitpro.org/packages/linux-musl/$arch/" > /etc/pacman.conf
+echo "[dkp-linux-musl]" >> /etc/pacman.conf
+echo "Server =  https://pkg.devkitpro.org/packages/linux-musl/$arch/" >> /etc/pacman.conf
 
 ## Install devkitPro pacman
 pacman -Syu
@@ -31,6 +27,4 @@ pacman -Syu
 dkp-pacman --noconfirm -S gba-dev
 
 # 2. Install a GBA emulator for debugging
-apk add flatpak
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install --assumeyes flathub io.mgba.mGBA
+apk add mgba
