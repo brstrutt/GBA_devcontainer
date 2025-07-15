4ffa6d35932b758958d3736a5ffff21c6ed9512f
brstrutt <ben.strutt@hotmail.co.uk>
2025-07-15 22:15:02 +0000

    fix minor issues with initial devcontainer install

--- .devcontainer/setup_environment.sh
+++ .devcontainer/setup_environment.sh
@@ -12,19 +12,20 @@ pacman-key --recv BC26F752D25B92CE272E0F44F7FD5492264BB9D0 --keyserver keyserver
 pacman-key --lsign BC26F752D25B92CE272E0F44F7FD5492264BB9D0
 
 pacman --noconfirm -U https://pkg.devkitpro.org/devkitpro-keyring.pkg.tar.zst
+pacman-key --populate devkitpro
 
 ## Add devkit pro repos
 echo "[dkp-libs]" >> /etc/pacman.conf
 echo "Server = https://pkg.devkitpro.org/packages" >> /etc/pacman.conf
 
 echo "[dkp-linux-musl]" >> /etc/pacman.conf
-echo "Server =  https://pkg.devkitpro.org/packages/linux-musl/$arch/" >> /etc/pacman.conf
+echo "Server =  https://pkg.devkitpro.org/packages/linux-musl/\$arch/" >> /etc/pacman.conf
 
 ## Install devkitPro pacman
 pacman -Syu
 
 ## finally install the GBA dev tools
-dkp-pacman --noconfirm -S gba-dev
+pacman --noconfirm -S gba-dev
 
 # 2. Install a GBA emulator for debugging
 apk add mgba
